9f84cddcb5915453dc88409b3f94f8d3f9b8c05f
gabrielTVasquez <gabrielvasquez@ufl.edu>
2023-11-21 00:27:42 -0500

    We now have an enemy
    he needs a lot more work but not rn

---
+++ enemy.gd
@@ -0,0 +1,16 @@
+extends Node2D
+var enemyHealth = 1200;
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	enemyHealth -= 1;
+	if enemyHealth <= 0:
+		queue_free();  #Supposed to delete the node
+	
+	pass

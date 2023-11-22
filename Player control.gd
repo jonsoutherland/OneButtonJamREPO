df65e1a11af283957b99d89c2478ec32b2a2cf20
jonsoutherland <j.southerland@ufl.edu>
2023-11-22 01:13:24 -0500

    Replaced Assets
    Replaced the stock photos with new ones, probably going to make better ones in the near future.

--- Player control.gd
+++ Player control.gd
@@ -6,16 +6,18 @@ var enemy_is_ready = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	player_is_drinking = Input.is_action_pressed("Iterate")
 	if player_is_drinking and enemy_is_ready:
+		$TextureProgressBar.value += 2
 		$Hand.set_visible(false)
 		$Drink.set_visible(true)
 	else:
+		$TextureProgressBar.value -= 1
 		$Hand.set_visible(true)
 		$Drink.set_visible(false)
 		
